0e505814c75ba99a3969f9281855dbfede821e2d
martin sisk <martin.sisk@pop.belmont.edu>
2020-04-30 11:04:50 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/* 
+Using the object above, console log "Our company's lawyer is Jeff Winger" using dot notation to access 'name'
+
+Console log "Jeff was hired on 09/22/2010" using bracket notation.
+
+Add a new key, vacation_days, and its value, 20, to employee. Use either dot or bracket notation. Does it matter which one you use? Try both to find out.
+
+let eom = "employee_of_the_month";
+Use the variable above to add a new property to employee. Set its value to false. Should you use dot or bracket notation?
+*/
+
+let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010"
+  }
+
+  let eom = "employee_of_the_month";
